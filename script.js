1c95ab99d61104e5f0796ffcb6b2817adf885908
SahilMaher <145198466+SahilMaher@users.noreply.github.com>
2024-02-19 18:59:53 +0530

    cm

---
+++ script.js
@@ -0,0 +1,10 @@
+// Toggle navigation menu for small screens
+function toggleMenu() {
+    var x = document.getElementById("navbar");
+    if (x.className === "navbar") {
+      x.className += " responsive";
+    } else {
+      x.className = "navbar";
+    }
+  }
+  
